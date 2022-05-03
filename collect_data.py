fcc041cbfe736db717c9f43e84e4d344d8fc5351
Kadir Burak Gündüz <73403802+kb1337@users.noreply.github.com>
2022-05-03 16:40:04 +0300

    Update coding style

--- collect_data.py
+++ collect_data.py
@@ -1,12 +1,16 @@
-from bs4 import BeautifulSoup
-from requests import get
-from aol_db import *
+"""Collects questions and answers from aolsoru.com"""
+
 import sys
 import os
 import re
+from requests import get
+from bs4 import BeautifulSoup
+from aol_db import aol_db
 
 
-def downloadMedia(url, file_name):
+def download_media(url: str, file_name: str) -> None:
+    """Downloads media from url to file_name"""
+
     print(f"\nDownloading {url} as {file_name}")
     with open(file_name, "wb") as file:
         response = get(url)
@@ -14,8 +18,10 @@ def downloadMedia(url, file_name):
     print(f"Download Successfull. {file_name}")
 
 
-def get_exams(url):
-    exams = dict()
+def get_exams(url: str) -> dict:
+    """Returns a dictionary of exam names and urls"""
+
+    exams = {}
     response = get(url, headers={"User-Agent": "Mozilla/5.0"}).content
     soup = BeautifulSoup(response, "html.parser")
     table = soup.find("table")
@@ -24,20 +30,22 @@ def get_exams(url):
     for row in rows:
         cols = row.find_all("td")
         for col in cols:
-            a = col.find_all("a")
-            if a:
-                for link in a:
+            a_tags = col.find_all("a")
+            if a_tags:
+                for link in a_tags:
                     year = cols[0].text.strip("\n").strip("\r")
                     exam_name = link.text[12::].replace(" ", "").strip("\n").strip("\r")
 
-                    exam_name = "{}_{}".format(year, exam_name)
+                    exam_name = f"{year}_{exam_name}"
                     exam_url = link.get("href")
                     exams[exam_name] = exam_url
     return exams
 
 
-def get_exam_details(url):
-    Q_A = dict()
+def get_exam_details(url: str) -> dict:
+    """Returns a dictionary of questions and answers"""
+
+    questions = {}
     response = get(url, headers={"User-Agent": "Mozilla/5.0"}).content
     soup = BeautifulSoup(response, "html.parser")
     for question in soup.find_all("div", {"class", "card text-lg-center"}):
@@ -50,45 +58,51 @@ def get_exam_details(url):
         else:
             img = img.get("src")
 
-        name = re.search("https:\/\/aolsoru\.com\/500\/(.*)", img).group(1)
-        downloadMedia(img, name)
+        name = re.search(r"https:\/\/aolsoru\.com\/500\/(.*)", img).group(1)
+        download_media(img, name)
         print(answer)
 
-        q = img[24::]
-        Q_A[q] = answer
-    return Q_A
+        questions[name] = answer
+    return questions
 
 
-url = ""
-lecture_name = ""
+def main() -> None:
+    """Main function"""
 
-if len(sys.argv) != 3:
-    sys.stderr.write(
-        "Missing Parameters\n\nUsage:\npython %s lacture_name url\n" % sys.argv[0]
-    )
-    sys.stderr.flush()
-    exit()
-else:
-    lecture_name = sys.argv[1]
-    url = sys.argv[2]
+    url = ""
+    lecture_name = ""
 
-base_url = "https://aolsoru.com"
+    if len(sys.argv) != 3:
+        sys.stderr.write(
+            f"Missing Parameters\n\nUsage:\npython {sys.argv[0]} lacture_name url\n"
+        )
+        sys.stderr.flush()
+        sys.exit()
+    else:
+        lecture_name = sys.argv[1]
+        url = sys.argv[2]
 
-db = aol_db()
-lecture_id = db.add_lecture(lecture_name)
+    base_url = "https://aolsoru.com"
 
-directory = "./static"
-if not os.path.exists(directory):
-    os.makedirs(directory)
-os.chdir(directory)
+    question_db = aol_db()
+    lecture_id = question_db.add_lecture(lecture_name)
 
-for name, path in get_exams(url).items():
-    print("\n{}\n{}\n".format(name, base_url + path))
-    exam_id = db.add_exam(lecture_id, name, base_url + path)
+    directory = "./static"
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    os.chdir(directory)
 
-    # Inserts questions to database at end of the exam. (every 20 questions in this case.)
-    for question, answer in get_exam_details(base_url + path).items():
-        db.add_question(exam_id, question, "", answer)
-    print("\n\n", "*" * 50)
+    for name, path in get_exams(url).items():
+        print(f"\n{name}\n{base_url + path}\n")
+        exam_id = question_db.add_exam(lecture_id, name, base_url + path)
 
-db.close_connection()
+        # Inserts questions to database at end of the exam. (every 20 questions in this case.)
+        for question, answer in get_exam_details(base_url + path).items():
+            question_db.add_question(exam_id, question, "", answer)
+        print("\n\n", "*" * 50)
+
+    question_db.close_connection()
+
+
+if __name__ == "__main__":
+    main()
